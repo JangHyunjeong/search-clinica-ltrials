5744d96adb7e8dc470897286cf5148168b2a6849
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-09-05 19:41:48 +0900

    feat: 검색 UI 마크업

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
 function App() {
-  return <div></div>
+  return (
+    <section>
+      <h2>검색하기</h2>
+      <p>
+        국내 모든 임상시험 검색하고
+        <br />
+        온라인으로 참여하기
+      </p>
+
+      <div>
+        <form>
+          <input type="text" />
+          <button type="button">검색하기</button>
+
+          <div style={{ display: 'none' }}>
+            <div>
+              <h3>추천 검색어</h3>
+
+              {/* TODO: 1. 검색어가 없을때 */}
+              <div>추천 검색어가 없습니다.</div>
+
+              {/* TODO: 2. 검색어가 있을때 */}
+              <ul>
+                <li>추천검색어1</li>
+                <li>추천검색어2</li>
+              </ul>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  )
 }
 
 export default App
