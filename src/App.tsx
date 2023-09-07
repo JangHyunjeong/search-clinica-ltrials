227443ff82eea466cf83ab07e05766d07b4d8a84
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-09-07 23:13:26 +0900

    refactor: Search 관련 코드 SearchPage로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,7 @@
-import { useState, useEffect } from 'react'
-
-import { ReconmmendKeywordType } from './components/search/types'
-import useDebounce from './hooks/useDebounce'
-import SearchPage from './page/searchPage'
+import SearchPage from './page/SearchPage'
 
 function App() {
-  const [keyword, setKeyword] = useState<string>('')
-  const debouncedKeyword = useDebounce(keyword)
-  const [reconmmendKeywords, setReconmmendKeywords] = useState<ReconmmendKeywordType[]>([])
-
-  const getInput = (value: string): void => {
-    setKeyword(value)
-  }
-  const clearInput = (): void => {
-    setKeyword('')
-  }
-
-  const getRecommendKeywords = async (debouncedKeyword: string) => {
-    const URL = `http://localhost:4000/sick/?q=${debouncedKeyword.trim().toLowerCase()}`
-    const cacheStorage = await caches.open('search')
-    const reponsedChache = await cacheStorage.match(URL)
-
-    try {
-      if (reponsedChache) {
-        const result = await reponsedChache.clone().json()
-        setReconmmendKeywords(result)
-        return
-      }
-
-      fetch(URL).then(async (response) => {
-        console.info('calling api')
-        const result = await response.clone().json()
-        setReconmmendKeywords(result)
-        await cacheStorage.put(URL, response)
-      })
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
-  useEffect(() => {
-    if (debouncedKeyword === '') {
-      setReconmmendKeywords([])
-      return
-    }
-
-    getRecommendKeywords(debouncedKeyword)
-  }, [debouncedKeyword])
-
-  return (
-    <SearchPage
-      clearInput={clearInput}
-      getInput={getInput}
-      keyword={keyword}
-      reconmmendKeywords={reconmmendKeywords}
-      setKeyword={setKeyword}
-    ></SearchPage>
-  )
+  return <SearchPage />
 }
 
 export default App
--- src/page/searchPage.tsx
+++ src/page/searchPage.tsx
@@ -1,8 +1,57 @@
+import { useState, useEffect } from 'react'
+
 import SearchForm from '../components/search/SearchForm'
 import SearchResult from '../components/search/SearchResult'
-import { SearchPageType } from '../components/search/types'
+import { ReconmmendKeywordType } from '../components/search/types'
+import useDebounce from '../hooks/useDebounce'
 
-function SearchPage(props: SearchPageType) {
+function SearchPage() {
+  const [keyword, setKeyword] = useState<string>('')
+  const debouncedKeyword = useDebounce(keyword)
+  const [reconmmendKeywords, setReconmmendKeywords] = useState<ReconmmendKeywordType[]>([])
+
+  const getInput = (value: string): void => {
+    setKeyword(value)
+  }
+  const clearInput = (): void => {
+    setKeyword('')
+  }
+
+  const getRecommendKeywords = async (debouncedKeyword: string) => {
+    const URL = `https://json-server-ashen-tau.vercel.app/sick/?q=${debouncedKeyword
+      .trim()
+      .toLowerCase()}`
+    const cacheStorage = await caches.open('search')
+    const reponsedChache = await cacheStorage.match(URL)
+
+    try {
+      if (reponsedChache) {
+        const result = await reponsedChache.clone().json()
+        setReconmmendKeywords(result)
+        return
+      }
+
+      fetch(URL).then(async (response) => {
+        console.info('calling api')
+        const result = await response.clone().json()
+        setReconmmendKeywords(result)
+        console.log(response)
+        await cacheStorage.put(URL, response)
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    if (debouncedKeyword === '') {
+      setReconmmendKeywords([])
+      return
+    }
+
+    getRecommendKeywords(debouncedKeyword)
+  }, [debouncedKeyword])
+
   return (
     <section>
       <h2>검색하기</h2>
@@ -13,16 +62,12 @@ function SearchPage(props: SearchPageType) {
       </p>
 
       <div>
-        <SearchForm
-          clearInput={props.clearInput}
-          getInput={props.getInput}
-          keyword={props.keyword}
-        ></SearchForm>
+        <SearchForm clearInput={clearInput} getInput={getInput} keyword={keyword}></SearchForm>
 
         <SearchResult
-          keyword={props.keyword}
-          reconmmendKeywords={props.reconmmendKeywords}
-          setKeyword={props.setKeyword}
+          keyword={keyword}
+          reconmmendKeywords={reconmmendKeywords}
+          setKeyword={setKeyword}
         ></SearchResult>
       </div>
     </section>
